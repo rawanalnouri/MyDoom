1dd0d59738233024171000c6af73b51112a82008
Rawan Alnouri <alnourirawan@gmail.com>
2023-01-25 18:34:32 +0000

    Create user interface including side menu, top menu and CSS styling

--- PersonalSpendingTracker/settings.py
+++ PersonalSpendingTracker/settings.py
@@ -119,6 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "myapp" / "static"]
+
+# User model for authentication purposes
+AUTH_USER_MODEL = 'ExpenseTracker.User'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
