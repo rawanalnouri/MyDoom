604f99d52ee6341124b9d66c9e3124493ad991ac
Rawan Alnouri <alnourirawan@gmail.com>
2023-01-25 18:36:13 +0000

    Create home and reports class-based views

--- ExpenseTracker/views.py
+++ ExpenseTracker/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from .forms import CategoryForm
 
-# Create your views here.
+class HomeView(View):
+    def get(self, request):
+        return render(request, 'home.html')
+
+class ReportsView(View):
+    def get(self, request):
+        return render(request, 'reports.html')
--- PersonalSpendingTracker/urls.py
+++ PersonalSpendingTracker/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ExpenseTracker.views import HomeView, ReportsView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name='home'),
+    path('reports/', ReportsView.as_view(), name='reports'),
 ]
