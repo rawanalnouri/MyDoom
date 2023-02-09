5201a87458d6612144402c89173f61519df9da40
9areeba <areeba-bito@hotmail.com>
2023-02-09 20:44:44 +0000

    Fix bugs

--- ExpenseTracker/tests/forms/testSignUpForm.py
+++ ExpenseTracker/tests/forms/testSignUpForm.py
@@ -2,35 +2,36 @@ from django import forms
 from django.test import TestCase
 from ExpenseTracker.forms import SignUpForm
 from ExpenseTracker.models import User
+from django.urls import reverse
 
 class SignUpFormTest(TestCase):
     def setUp(self):
         self.input = {
-            "first_name": "Bob",
-            "last_name": "White",
+            "firstName": "Bob",
+            "lastName": "White",
             'username': 'bob123',
             "email": "bobwhite@email.org",
-            "new_password": "Password123",
-            "password_confirmation": "Password123"
+            "newPassword": "Password123",
+            "passwordConfirmation": "Password123"
         }
         self.form = SignUpForm(data=self.input)
 
     def testFormHasCorrectFields(self):
-        self.assertIn('first_name', self.form.fields)
-        self.assertIn('last_name', self.form.fields)
+        self.assertIn('firstName', self.form.fields)
+        self.assertIn('lastName', self.form.fields)
         self.assertIn('username', self.form.fields)
         self.assertIn('email', self.form.fields)
-        self.assertIn('new_password', self.form.fields)
-        self.assertIn('password_confirmation', self.form.fields)
+        self.assertIn('newPassword', self.form.fields)
+        self.assertIn('passwordConfirmation', self.form.fields)
         self.assertTrue(self.form.fields['email'], forms.EmailField)
-        self.assertTrue(self.form.fields['new_password'].widget, forms.PasswordInput)
-        self.assertTrue(self.form.fields['password_confirmation'].widget, forms.PasswordInput)
+        self.assertTrue(self.form.fields['newPassword'].widget, forms.PasswordInput)
+        self.assertTrue(self.form.fields['passwordConfirmation'].widget, forms.PasswordInput)
 
     def testValidSignUp(self):
         self.assertTrue(self.form.is_valid())
 
     def testBothPasswordsAreEqual(self):
-        self.input['password_confirmation'] = 'WrongPassword1234'
+        self.input['passwordConfirmation'] = 'WrongPassword1234'
         form = SignUpForm(data = self.input)
         self.assertFalse(form.is_valid())
 
@@ -42,19 +43,35 @@ class SignUpFormTest(TestCase):
         self.input['email'] = ''
         self.assertFalse(SignUpForm(data=self.input).is_valid())
 
+    def testEmailHasToBeUnique(self):
+        self.client.post(reverse('signUp'), self.input)
+        #change other unique field so should fail at fact email not unique
+        self.input['username'] = "9bob" 
+        self.assertFalse(SignUpForm(data=self.input).is_valid())
+
     def testFormDisallowsPasswordsOfLengthLessThan8(self):
-        self.input['new_password'] = 'test'
-        self.input['password_confirmation'] = 'test'
+        self.input['newPassword'] = 'test'
+        self.input['passwordConfirmation'] = 'test'
         self.assertFalse(SignUpForm(data=self.input).is_valid())
 
     def testFromRejectsWhenPasswordsAreDifferent(self):
         self.input['new_password'] = 'Password123'
-        self.input['password_confirmation'] = 'Password1234'
+        self.input['passwordConfirmation'] = 'Password1234'
         self.assertFalse(SignUpForm(data=self.input).is_valid())
 
 
-    # def testPasswordNeedsAnUpperCase(self):
+    def testPasswordNeedsAnUpperCase(self):
+        self.input['newPassword'] = 'password123'
+        self.input['passwordConfirmation'] = 'password1234'
+        self.assertFalse(SignUpForm(data=self.input).is_valid())
 
-    # def testPasswordNeedsANumber(self):
-
-    # def testUsernameHasToBeUnique(self):
+    def testPasswordNeedsANumber(self):
+        self.input['newPassword'] = 'password'
+        self.input['passwordConfirmation'] = 'password'
+        self.assertFalse(SignUpForm(data=self.input).is_valid())
+ 
+    def testUsernameHasToBeUnique(self):
+        self.client.post(reverse('signUp'), self.input)
+        #change other unique field so should fail at fact username not unique
+        self.input['email'] = "bob@email.com"
+        self.assertFalse(SignUpForm(data=self.input).is_valid())
