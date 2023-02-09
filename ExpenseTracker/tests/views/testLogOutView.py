a821b9d2f3fe1586154bf0b53bab216b58e2127f
9areeba <areeba-bito@hotmail.com>
2023-02-09 00:56:04 +0000

    Add logOut view tests

---
+++ ExpenseTracker/tests/views/testLogOutView.py
@@ -0,0 +1,27 @@
+from ExpenseTracker.models import User 
+from django.test import TestCase
+from django.urls import reverse
+from django.contrib import auth
+from django.contrib import messages
+from ExpenseTracker.tests.helpers import LogInTester
+
+class TestLogOut(TestCase, LogInTester):
+    fixtures = ['ExpenseTracker/tests/fixtures/defualt_objects.json']
+
+    def setUp(self):
+        self.url = reverse('logOut')
+        self.user = User.objects.get(id=1)
+
+    def testUrl(self):
+        self.assertEqual(self.url,'/logOut/')
+
+    def testLogOutAndRedirect(self):
+        self.client.login(username='bob123', password='Password123')
+        self.assertTrue(self.isUserLoggedIn())
+
+        #Get log out view and check if user has been succesfully logged out
+        response = self.client.get(self.url, follow=True)
+        response_url = reverse('index')
+        self.assertRedirects(response, response_url, status_code=302, target_status_code=200)
+        self.assertTemplateUsed(response, 'index.html')
+        self.assertFalse(self.isUserLoggedIn())
