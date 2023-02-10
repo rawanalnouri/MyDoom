04ba8afc1e95985ace746e1502b9834a9bc96fc4
9areeba <areeba-bito@hotmail.com>
2023-02-10 22:01:01 +0000

    Create tests for update and delete categories
    Co-authored-by: Ruhan Mohammad Salam <k21056749@kcl.ac.uk>

--- ExpenseTracker/tests/views/testCategoryView.py
+++ ExpenseTracker/tests/views/testCategoryView.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 from ExpenseTracker.models import User, Category, Expenditure, SpendingLimit
-from ExpenseTracker.forms import ExpenditureForm
+from ExpenseTracker.forms import ExpenditureForm, CategorySpendingLimitForm
 from django.core.paginator import Page
 import datetime
 
@@ -11,10 +11,7 @@ class CategoryViewTest(TestCase):
     def setUp(self):
         self.user = User.objects.get(id=1)
         self.client.force_login(self.user)
-        # self.expenditure = Expenditure.objects.create(title='testexpenditure', date=datetime.date.today(), amount=10)
         self.expenditure = Expenditure.objects.get(id=1)
-        # spendingLimit = SpendingLimit.objects.create(amount='20', timePeriod='daily')
-        # self.category = Category.objects.create(name='testcategory', spendingLimit=spendingLimit, user=self.user)
         self.category = Category.objects.get(id=1)
         self.user.categories.add(self.category)
         self.category.expenditures.add(self.expenditure)
@@ -27,7 +24,7 @@ class CategoryViewTest(TestCase):
         self.assertEqual(response.context['category'], self.category)
         self.assertIsInstance(response.context['expenditures'], Page)
 
-    def testCategoryViewPost(self):
+    def testCreateExpenditure(self):
         data = {'expenditureForm': 'True', 'title': 'testexpenditure2', 'date': datetime.date.today(), 'amount': 10}
         response = self.client.post(reverse('category', args=[self.category.id]), data)
         self.assertEqual(response.status_code, 302)
@@ -35,13 +32,31 @@ class CategoryViewTest(TestCase):
         self.assertEqual(Expenditure.objects.count(), 2)
         self.assertTrue(Expenditure.objects.filter(title='testexpenditure2').exists())
     
-    def testCategoryViewPostWithInvalidData(self):
+    def testCreateExpenditureWithInvalidData(self):
         data = {'title': '', 'date': '', 'amount': ''}
         response = self.client.post(reverse('category', args=[self.category.id]), data)
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.url, reverse('category', args=[self.category.id]))
         self.assertEqual(Expenditure.objects.count(), 1)
         self.assertEqual(Expenditure.objects.first().title, 'testexpenditure')
+
+    def testCategoryUpdatesCorrectly(self):
+        data = {
+            'categoryForm': 'True', 
+            'name': 'Updated Title', 
+            'description': 'Updated Description', 
+            'timePeriod': 'monthly',
+            'amount': 100
+            }
+        response = self.client.post(reverse('category', args=[self.category.id]), data)
+        updatedCategory = Category.objects.get(id=self.category.id)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, reverse('category', args=[self.category.id]))
+        self.assertEqual(updatedCategory.name, 'Updated Title')
+        self.assertEqual(updatedCategory.description, 'Updated Description')
+        self.assertEqual(updatedCategory.spendingLimit.timePeriod, 'monthly')
+        self.assertEqual(updatedCategory.spendingLimit.amount, 100)
+
 
     def testCategoryViewPagination(self):
         # Create additional expenditures
