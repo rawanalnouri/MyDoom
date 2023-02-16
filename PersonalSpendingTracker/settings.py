6f79fd808d4b3e11729aaa3478813eeb8c22cc5f
9areeba <areeba-bito@hotmail.com>
2023-02-16 23:24:43 +0000

    Update htmls to show notifications in nabar dropdown

--- PersonalSpendingTracker/settings.py
+++ PersonalSpendingTracker/settings.py
@@ -63,8 +63,8 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages'
-                #'PersonalSpendingTracker.contextProcessor.getNotifications'
+                'django.contrib.messages.context_processors.messages',
+                'ExpenseTracker.contextProcessor.getNotifications'
             ],
         },
     },
