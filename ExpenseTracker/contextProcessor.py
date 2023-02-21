d103fac20d665d7f7af215fe10c5f7749094dd13
9areeba <areeba-bito@hotmail.com>
2023-02-21 15:37:14 +0000

    Add new context for latest three notifications
    Co-authored-by: Ruhan Mohammad Salam <k21056749@kcl.ac.uk>

--- ExpenseTracker/contextProcessor.py
+++ ExpenseTracker/contextProcessor.py
@@ -9,6 +9,7 @@ def getNotifications(request):
         unreadNotifications = Notification.objects.filter(user = request.user, isSeen = False)
         readNotifications = Notification.objects.filter(user = request.user, isSeen = True)
         context['unreadNotifications'] = unreadNotifications
+        context['latestNotifications'] = unreadNotifications[:3]
         context['readNotifications'] = readNotifications
     
     return context
