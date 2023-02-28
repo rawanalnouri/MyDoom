a629bf8fd399202d6f23986026dcd6f8f9706d2d
9areeba <areeba-bito@hotmail.com>
2023-02-28 23:21:25 +0000

    Make new functions for creating notifications based on their types
    Co-authored-by: Ruhan Mohammad Salam <k21056749@kcl.ac.uk>

--- ExpenseTracker/helpers/utils.py
+++ ExpenseTracker/helpers/utils.py
@@ -1,9 +1,31 @@
 from ExpenseTracker.models import *
 
-# Function to create a notifcation for a user 
-def createNotification(user, title, message):
+# Function to create a basic notifcation for a user 
+def createBasicNotification(toUser, title, message):
     Notification.objects.create(
-    user=user,
+    toUser=toUser,
     title=title,
-    message=message
+    message=message,
+    type='basic'
+    )
+
+# Function to create a share category notifcation for a user 
+def createShareCategoryNotification(toUser, title, message, sharedCategory, fromUser):
+    ShareCategoryNotification.objects.create(
+    toUser=toUser,
+    fromUser = fromUser,
+    title=title,
+    message=message,
+    sharedCategory = sharedCategory,
+    type='category'
+    )
+
+# Function to create a share category notifcation for a user 
+def createFriendRequestNotification(toUser, title, message, fromUser):
+    ShareCategoryNotification.objects.create(
+    toUser=toUser,
+    fromUser = fromUser,
+    title=title,
+    message=message,
+    type='friend'
     )
