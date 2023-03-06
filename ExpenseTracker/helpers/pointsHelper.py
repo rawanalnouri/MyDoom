bd90833c643ff8bd6a693382f8aac8b9e7310103
Imaan <72647431+imaan244@users.noreply.github.com>
2023-03-06 15:08:21 +0000

    Add more functionality for points
    Co-Authored-By: Katbenn <95385281+Katbenn@users.noreply.github.com>

--- ExpenseTracker/helpers/pointsHelper.py
+++ ExpenseTracker/helpers/pointsHelper.py
@@ -1,6 +1,7 @@
 
-from ExpenseTracker.models import Points
-from django.utils.timezone import datetime
+from ExpenseTracker.models import Points, Category, SpendingLimit, Expenditure
+from django.utils.timezone import datetime, timedelta
+#from datetime.datetime import now
 
 def addPoints(request,n):
     user = request.user
@@ -9,6 +10,83 @@ def addPoints(request,n):
     points = Points.objects.get(user=request.user).pointsNum
     pointsObject.pointsNum = points+n
     pointsObject.save()
+
+def getTimePeriod(request,category):
+    user = request.user
+    # need a list of all categories and then do a for loop through them all 
+    # categories = Category.objects.all()
+    currentSpendingLimit = Category.objects.get(id=category.id).spendingLimit
+    period = currentSpendingLimit.timePeriod
+    return period
+
+def getTodaySpending(request,category):
+   
+    currentCategory = Category.objects.get(id=category.id)
+    spent=0.00
+    for expence in currentCategory.expenditures.filter(date=datetime.now().date()):
+        spent+=float(expence.amount)
+    return spent
+       
+
+def dailyTracking(request,category):
+    currentCategory = Category.objects.get(id=category.id)
+    spent = getTodaySpending(request,category)
+    if abs(currentCategory.spendingLimit.amount) >= abs(spent):
+        addPoints(request,5)
+
+def weeklyTracking(request,category):
+    currentCategory = Category.objects.get(id=category.id)
+    spent=0.0
+    for expence in currentCategory.expenditures.filter(createdAt__gte=datetime.now()-timedelta(days=7)):
+        spent+=float(expence.amount)
+    if abs(currentCategory.spendingLimit.amount) >= abs(spent):
+        addPoints(request,5)
+    else:
+        addPoints(request,-5)
+
+def monthlyTracking(request,category):
+    currentCategory = Category.objects.get(id=category.id)
+    spent=0.0
+    for expence in currentCategory.expenditures.filter(createdAt__gte=datetime.now()-timedelta(months=1)):
+        spent+=float(expence.amount)
+    if abs(currentCategory.spendingLimit.amount) >= abs(spent):
+        addPoints(request,5)
+    
+    
+
+# def checkExpenditure(request,category,expenditure):
+#     currentCategory = Category.objects.get(id=category.id)
+#     spent = Expenditure.objects.get(id=expenditure.id).amount
+
+def trackPoints(request):
+    date = datetime.now()
+    for category in Category.objects.filter(users=request.user):
+        
+        if category.spendingLimit.timePeriod=='daily':
+            dailyTracking(request,category)
+        elif category.spendingLimit.timePeriod=='weekly' and date.weekday()==0:
+            weeklyTracking(request,category)
+        elif category.spendingLimit.timePeriod=='monthly' and date.date() == date.replace(day=1).date():
+            #check if its the first day of the month
+            monthlyTracking(request,category)
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
