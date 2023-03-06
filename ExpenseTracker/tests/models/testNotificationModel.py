a852fc3983c4ae748e6354f57d270a9eb67fd53f
Ruhan Mohammad Salam <k21056749@kcl.ac.uk>
2023-03-06 16:35:48 +0000

    Fix notification model tests

--- ExpenseTracker/tests/models/testNotificationModel.py
+++ ExpenseTracker/tests/models/testNotificationModel.py
@@ -1,74 +1,98 @@
-# from ExpenseTracker.models import Notification, User
-# from django.test import TestCase
-# from datetime import date
-# from django.core.exceptions import ValidationError
+from ExpenseTracker.models import Notification, User
+from django.test import TestCase
+from datetime import date
+from django.core.exceptions import ValidationError
 
 
-# class NotificationModelTestCase(TestCase):
-#     def setUp(self):
-#         firstName = 'John'
-#         lastName = 'Doe'
-#         email = f'{firstName}.{lastName}@example.org'
-#         username = 'johndoe'
-#         password = "Password123"
-#         self.user = User.objects.create(
-#             username = username,
-#             firstName = firstName,
-#             lastName = lastName,
-#             email = email,
-#             password = password
-#         )
+class NotificationModelTestCase(TestCase):
+    def setUp(self):
 
-#         self.notification = Notification.objects.create(
-#             user = self.user,
-#             title = "Test Title",
-#             message = "This is a notification message text for test purposes."
-#         )
+        #For basic notification model
+        firstName = 'John'
+        lastName = 'Doe'
+        email = 'testEmail1@example.org'
+        username = 'johndoe'
+        password = "Password123"
+        self.user = User.objects.create(
+            username = username,
+            firstName = firstName,
+            lastName = lastName,
+            email = email,
+            password = password
+        )
 
-#     def testNotificationIsValid(self):
-#         try:
-#             self.notification.full_clean()
-#         except:
-#             self.fail('Notification must be valid')
+        self.notification = Notification.objects.create(
+            toUser = self.user,
+            title = "Test Title",
+            message = "This is a notification message text for test purposes.",
+            type = 'basic'
+        )
 
-#     def testNoBlankUser(self):
-#         self.notification.user = None
-#         with self.assertRaises(ValidationError):
-#             self.notification.full_clean()
+        #For extended notifications
+        firstName2 = 'Jane'
+        lastName2 = 'Smith'
+        email2 = 'testEmail2@example.org'
+        username2 = 'janesmith'
+        password2 = "Password123"
+        self.user2 = User.objects.create(
+            username = username2,
+            firstName = firstName2,
+            lastName = lastName2,
+            email = email2,
+            password = password2
+        )
 
-#     def testNoBlankTitle(self):
-#         self.notification.title = None
-#         with self.assertRaises(ValidationError):
-#             self.notification.full_clean()
+        self.notification2 = Notification.objects.create(
+            toUser = self.user,
+            title = "Test Title",
+            message = "This is a notification message text for test purposes.",
+            type = 'basic'
+        )
 
-#     def testNoBlankMessage(self):
-#         self.notification.message = None
-#         with self.assertRaises(ValidationError):
-#             self.notification.full_clean()
+    def testNotificationIsValid(self):
+        try:
+            self.notification.full_clean()
+        except:
+            self.fail('Notification must be valid')
 
-#     def testNoBlankIsSeenField(self):
-#         self.notification.isSeen = None
-#         with self.assertRaises(ValidationError):
-#             self.notification.full_clean()
+    def testNoBlankUser(self):
+        self.notification.toUser = None
+        with self.assertRaises(ValidationError):
+            self.notification.full_clean()
 
-#     def testNoBlankDate(self):
-#         self.notification.createdAt = None
-#         with self.assertRaises(ValidationError):
-#             self.notification.full_clean()
+    def testNoBlankTitle(self):
+        self.notification.title = None
+        with self.assertRaises(ValidationError):
+            self.notification.full_clean()
 
-#     def testTitleWithinLengthLimit(self):
-#         self.notification.title = "TCI5h49Wc6OLthsThldTZ3VKXxt3EhlEdcZgZJLYmnH4MOciYXqR41433LrOdBL5JU0te7RPRzNgyTxN3eBDBnl4osIWDLRHwmva0FBWZQYPGWDRdrN78mXYPwjBlz4HxKL9u59bvKOcGQ6sGDIedqY0GPprjoa1Yk9FiMbbhWXuRff0r4dftrwECyM7uCtyeNFxrD5BXEROrANuajTkgKIQI8IcpiezguQaxl0q8eXOFTb2Ix5M0YMhTzBhHa2s0YXI"
-#         with self.assertRaises(ValidationError):
-#             self.notification.full_clean()
+    def testNoBlankMessage(self):
+        self.notification.message = None
+        with self.assertRaises(ValidationError):
+            self.notification.full_clean()
 
-#     def testMessageWithinLengthLimit(self):
-#         self.notification.price = "TCI5h49Wc6OLthsThldTZ3VKXxt3EhlEdcZgZJLYmnH4MOciYXqR41433LrOdBL5JU0te7RPRzNgyTxN3eBDBnl4osIWDLRHwmva0FBWZQYPGWDRdrN78mXYPwjBlz4HxKL9u59bvKOcGQ6sGDIedqY0GPprjoa1Yk9FiMbbhWXuRff0r4dftrwECyM7uCtyeNFxrD5BXEROrANuajTkgKIQI8IcpiezguQaxl0q8eXOFTb2Ix5M0YMhTzBhHa2s0YXI"
-#         with self.assertRaises(ValidationError):
-#             self.notification.full_clean()
+    def testNoBlankIsSeenField(self):
+        self.notification.isSeen = None
+        with self.assertRaises(ValidationError):
+            self.notification.full_clean()
 
-#     def testUserExists(self):
-#         self.user.save()
-#         user = User.objects.get(email = self.notification.user.email)
-#         self.assertEqual(user, self.notification.user)
+    def testNoBlankType(self):
+        self.notification.type = None
+        with self.assertRaises(ValidationError):
+            self.notification.full_clean()
 
-#     # def testCreatedDateIsNow
+    def testTitleWithinLengthLimit(self):
+        self.notification.title = "TCI5h49Wc6OLthsThldTZ3VKXxt3EhlEdcZgZJLYmnH4MOciYXqR41433LrOdBL5JU0te7RPRzNgyTxN3eBDBnl4osIWDLRHwmva0FBWZQYPGWDRdrN78mXYPwjBlz4HxKL9u59bvKOcGQ6sGDIedqY0GPprjoa1Yk9FiMbbhWXuRff0r4dftrwECyM7uCtyeNFxrD5BXEROrANuajTkgKIQI8IcpiezguQaxl0q8eXOFTb2Ix5M0YMhTzBhHa2s0YXI"
+        with self.assertRaises(ValidationError):
+            self.notification.full_clean()
+
+    def testMessageWithinLengthLimit(self):
+        self.notification.message = "TCI5h49Wc6OLthsThldTZ3VKXxt3EhlEdcZgZJLYmnH4MOciYXqR41433LrOdBL5JU0te7RPRzNgyTxN3eBDBnl4osIWDLRHwmva0FBWZQYPGWDRdrN78mXYPwjBlz4HxKL9u59bvKOcGQ6sGDIedqY0GPprjoa1Yk9FiMbbhWXuRff0r4dftrwECyM7uCtyeNFxrD5BXEROrANuajTkgKIQI8IcpiezguQaxl0q8eXOFTb2Ix5M0YMhTzBhHa2s0YXIa;lkdfj"
+        with self.assertRaises(ValidationError):
+            self.notification.full_clean()
+
+    def testUserExists(self):
+        self.user.save()
+        user = User.objects.get(email = self.notification.toUser.email)
+        self.assertEqual(user, self.notification.toUser)
+
+    #def testCreatedDateIsNow
