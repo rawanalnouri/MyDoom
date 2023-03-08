87a4649581dd065f53a2143a04cf63bb573eca63
9areeba <areeba-bito@hotmail.com>
2023-03-08 17:29:56 +0000

    Add missing tests fro index view

--- ExpenseTracker/tests/views/testIndexView.py
+++ ExpenseTracker/tests/views/testIndexView.py
@@ -1,7 +1,20 @@
 from django.test import TestCase
-
+from django.urls import reverse
+from ExpenseTracker.models import User
 class IndexViewTest(TestCase):
-    def testIndexView(self):
+
+    fixtures = ['ExpenseTracker/tests/fixtures/defaultObjects.json']
+
+    def setUp(self):
+        self.user = User.objects.get(id=1)
+        self.client.force_login(self.user)
+
+    def testGetIndexViewWhenNotLoggedIn(self):
+        self.client.logout()
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
+
+    def testRedirectsToHomeIfLoggedIn(self):
+        response = self.client.get(reverse('index'))
+        self.assertRedirects(response, reverse('home'))
