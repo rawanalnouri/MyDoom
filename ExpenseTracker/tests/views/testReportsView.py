b06c865d795a3d3cc1fddbf3d21d7c3b707a6e97
Zahra Amaan <k21011879@kcl.ac.uk>
2023-03-13 14:04:07 +0000

    update testReportsView.py
    add tests for the reports page
    
    Co-Authored-By: irocketi <99259424+irocketi@users.noreply.github.com>

--- ExpenseTracker/tests/views/testReportsView.py
+++ ExpenseTracker/tests/views/testReportsView.py
@@ -1,34 +1,35 @@
-# from django.contrib import messages
-# from django.test import TestCase
-# from django.urls import reverse
-# from ExpenseTracker.forms import ReportForm
-# from ExpenseTracker.models import *
+from django.contrib import messages
+from django.test import TestCase
+from django.urls import reverse
+from ExpenseTracker.forms import ReportForm
+from ExpenseTracker.models import *
 
 
-# class ReportViewTest(TestCase):
+class ReportViewTest(TestCase):
 
      
-#     fixtures = ['ExpenseTracker/tests/fixtures/defaultObjects.json']
+    fixtures = ['ExpenseTracker/tests/fixtures/defaultObjects.json']
 
 
-#     def setUp(self):
-#         self.user = User.objects.get(id=1)
+    def setUp(self):
+        self.user = User.objects.get(id=1)
+        self.client.force_login(self.user)
         
 
 
-# #  testing if page is correctly loaded
-# # with the correct form 
-# # and with the correct template 
-#     def testCategoryReportViewGet(self):
-#         response = self.client.get(reverse('reports'))
-#         self.assertEqual(response.status_code, 200)
-#         self.assertTemplateUsed(response, 'reports.html')
-#         self.assertIsInstance(response.context['form'], ReportForm)
+#  testing if page is correctly loaded
+# with the correct form 
+# and with the correct template 
+    def testCategoryReportViewGet(self):
+        response = self.client.get(reverse('reports'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'reports.html')
+        self.assertIsInstance(response.context['form'], ReportForm)
 
 
-#     def testHomeViewRedirectsToLoginIfNotLoggedIn(self):
-#         self.client.logout()
-#         response = self.client.get(reverse('home'))
-#         self.assertRedirects(response, '/logIn/')
+    def testHomeViewRedirectsToLoginIfNotLoggedIn(self):
+        self.client.logout()
+        response = self.client.get(reverse('home'))
+        self.assertRedirects(response, '/logIn/')
 
     
