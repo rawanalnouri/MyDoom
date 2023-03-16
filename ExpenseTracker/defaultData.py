62fca498b47fcc747c2cb92f772579affc783f12
Rawan Alnouri <alnourirawan@gmail.com>
2023-03-16 13:00:39 +0000

    Remake 'seed' to include houses and put users in houses

--- ExpenseTracker/defaultData.py
+++ ExpenseTracker/defaultData.py
@@ -1,9 +1,6 @@
-from ExpenseTracker import models
-
 DEFAULT_HOUSES = [
-    {'id': 1, 'points':0,'house':'Sycamore','numOfMembers': 0},
-    {'id': 2,'points':0,'house':'Oak','numOfMembers':0},
-    {'id':3,'points':0,'house':'Cherry Blossom','numOfMembers':0},
-    {'id':4,'points':0,'house':'Willow','numOfMembers':0},
-
+    {'id': 1, 'points':0, 'title':'Sycamore','memberCount': 0},
+    {'id': 2,'points':0, 'title':'Oak','memberCount':0},
+    {'id':3,'points':0, 'title':'Cherry Blossom','memberCount':0},
+    {'id':4,'points':0, 'title':'Willow','memberCount':0},
 ]
