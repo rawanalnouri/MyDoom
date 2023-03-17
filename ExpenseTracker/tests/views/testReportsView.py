99ebfb616b4d6d345d55c070633060e19dd6bdbf
Zahra Amaan <k21011879@kcl.ac.uk>
2023-03-17 23:33:49 +0000

    refactor tests for views
    Co-Authored-By: imaan244 <72647431+imaan244@users.noreply.github.com>
    Co-Authored-By: Katbenn <95385281+Katbenn@users.noreply.github.com>

--- ExpenseTracker/tests/views/testReportsView.py
+++ ExpenseTracker/tests/views/testReportsView.py
@@ -4,58 +4,163 @@
 # from ExpenseTracker.forms import ReportForm   
 # from ExpenseTracker.models import *   
 
-
-
-
-
-
-
 # class ReportViewTest(TestCase):  
 
-     
+#   fixtures = ['ExpenseTracker/tests/fixtures/defaultObjects.json']     
 
-
-
-#     fixtures = ['ExpenseTracker/tests/fixtures/defaultObjects.json']     
-
-
-
-
-
-
-#     def setUp(self):    
-#         self.user = User.objects.get(id=1)    
+#    def setUp(self):   
+#         self.user = User.objects.get(id=1)   
         
+#  testing if page is correctly loaded.
 
+# # with the correct form.
 
+# # and with the correct template.
 
+#     def testCategoryReportViewGet(self): 
+#         response = self.client.get(reverse('reports'))  
+#         self.assertEqual(response.status_code, 200)    
+#         self.assertTemplateUsed(response, 'reports.html')  
+#         self.assertIsInstance(response.context['form'], ReportForm)   
 
-
-
-
-# #  testing if page is correctly loaded.
-
-# # with the correct form .
-
-# # and with the correct template .
-
-#     def testCategoryReportViewGet(self):  
-#         response = self.client.get(reverse('reports'))   
-#         self.assertEqual(response.status_code, 200)     
-#         self.assertTemplateUsed(response, 'reports.html')   
-#         self.assertIsInstance(response.context['form'], ReportForm)    
-
-
-
-
-
-
-#     def testHomeViewRedirectsToLoginIfNotLoggedIn(self):    
-#         self.client.logout()   
-#         response = self.client.get(reverse('home'))   
-#         self.assertRedirects(response, '/logIn/')   
+#     def testHomeViewRedirectsToLoginIfNotLoggedIn(self):   
+#         self.client.logout()  
+#         response = self.client.get(reverse('home'))  
+#         self.assertRedirects(response, '/logIn/')  
 
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
