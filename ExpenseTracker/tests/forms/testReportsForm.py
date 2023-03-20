f13552fdfa9c14340dc8e86c943501db8a7954b8
Sabeeka <itsabeeka@gmail.com>
2023-03-20 14:25:55 +0000

    refactor forms and add tests
    Co-Authored-By: z03a <99799823+z03a@users.noreply.github.com>

---
+++ ExpenseTracker/tests/forms/testReportsForm.py
@@ -0,0 +1,12 @@
+# from django import forms
+# from django.test import TestCase
+# from ExpenseTracker.forms import ReportForm
+#
+# class ReportsFormTest(TestCase):
+#     fixtures = ['ExpenseTracker/tests/fixtures/defaultObjects.json']
+#
+#     def setUp(self):
+#         self.user = User.Objects.get(id=1)
+#         self.client.force_login(self.user)
+#
+#         self.form = ReportForm(data=self.input)
